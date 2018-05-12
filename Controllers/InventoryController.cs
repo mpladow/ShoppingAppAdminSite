d8c5cd7e6d35b4d693120347850c90b05c395775
Michael L <mpladow@gmail.com>
2018-05-12 19:05:56 +1000

    ProductList
    added productList index

---
+++ Controllers/InventoryController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineStore_ADMIN.Entities;
+using OnlineStore_ADMIN.ViewModels;
+
+namespace OnlineStore_ADMIN.Controllers
+{
+    public class InventoryController : Controller
+    {
+        OrderingSystemEntities1 os = new OrderingSystemEntities1();
+        // GET: Inventory
+        public ActionResult Index()
+        {
+            var inventory = os.Products
+                .Where(x => x.DeletedAt == null)
+                .ToList();
+            return View(inventory);
+        }
+        public ActionResult Edit(int id)
+        {
+            var productInDb = os.Products
+                .FirstOrDefault(x => x.OrderID == id);
+            if (productInDb == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                InventoryVM vm = new InventoryVM()
+                {
+
+                }
+                return View(productInDb);
+            }
+        }
+    }
+}
---
+++ ViewModels/InventoryVM.cs
@@ -0,0 +1,26 @@
+﻿using OnlineStore_ADMIN.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineStore_ADMIN.ViewModels
+{
+    public class InventoryVM
+    {
+        public int ProductID { get; set; }
+        public Nullable<int> OrderID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Nullable<int> Quantity { get; set; }
+        public Nullable<System.DateTime> DeletedAt { get; set; }
+        public Nullable<int> Price { get; set; }
+        public bool IsStocked { get; set; }
+
+        public string Heading { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+        public virtual Product Products1 { get; set; }
+        public virtual Product Product1 { get; set; }
+    }
+}
